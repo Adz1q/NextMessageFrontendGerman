5a38b7620c730332e90df375a9f09e1c0f4f574b
Adz1q <mikolajs2211@gmail.com>
2025-06-01 21:31:09 +0200

    csa

--- src/components/team-message-card/team-message-card.tsx
+++ src/components/team-message-card/team-message-card.tsx
@@ -22,7 +22,7 @@ export default function TeamMessageCard({ message, userId }: TeamMessageDetails)
             {isDisplayed && <div className="flex w-full items-center justify-center text-foreground">
                 {displayedDate}
             </div>}
-            <div className="flex items-center justify-center text-foreground">{message.senderUsername}</div>
+            <div className={`${message.senderId === userId ? "pr-1" : "pl-1"} flex items-center justify-center text-foreground`}>{message.senderUsername}</div>
             <div className="bg-foreground rounded-md text-muted pr-3 pl-3 pt-1 pb-1 text-md">
                 {message.content}
             </div>
